6df6830a824b8b625ca73e3bcd7d4ac6cf951c45
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-07-28 15:02:28 +0100

    update

--- check_path.c
+++ check_path.c
@@ -10,37 +10,37 @@ void check_path(shell *session)
 	int i = 1, len, a = 0;
 	char command[BUFF_MAX], *string = NULL;
 	char *pathcopy = duplicate(session->pathway);
-	char *token = tokenize(pathcopy, ":");
-	char *str = session->args[0];
+	char *token = tokenize(pathcopy, ":"), *str = session->args[0];
 
 	i = access(str, X_OK);
 	while (a < BUFF_MAX)
 		setnull(command, a++);
 	if (!i)
-		session->full_path = duplicate(str);
-	else
 	{
-		while (token)
+		session->full_path = duplicate(str);
+		free(pathcopy);
+		return;
+	}
+	while (token)
+	{
+		len = length(token) + length(str) + 2;
+		catenate(command, token);
+		catenate(command, "/");
+		catenate(command, str);
+		setnull(command, (len - 1));
+		i = access(command, X_OK);
+		if (!i)
 		{
-			len = length(token) + length(str) + 2;
-			catenate(command, token);
-			catenate(command, "/");
-			catenate(command, str);
-			setnull(command, (len - 1));
-			i = access(command, X_OK);
-			if (!i)
-			{
-				string = malloc(sizeof(char) * (length(command) + 1));
-				copy(string, command);
-				session->full_path = string;
-				free(pathcopy);
-				if (string == NULL)
-					break;
-				return;
-			}
-			setnull(command, 0);
-			token = tokenize(NULL, ":");
+			string = malloc(sizeof(char) * (length(command) + 1));
+			copy(string, command);
+			session->full_path = string;
+			free(pathcopy);
+			if (string == NULL)
+				break;
+			return;
 		}
+		setnull(command, 0);
+		token = tokenize(NULL, ":");
 	}
 	free(pathcopy);
 	session->full_path = duplicate(str);
